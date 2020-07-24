b1b55d88afbac4aa05470debf487d795ec1dc1f6
wingchiehpih <wingchiehpih@gmail.com>
2020-07-24 14:50:09 +0800

    初始化地图组件

---
+++ src/views/amap/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="amap-wrap">
+    <el-amap vid="amapContainer"> </el-amap>
+  </div>
+</template>
+
+<script>
+import { AMapManager, lazyAMapApiLoaderInstance } from 'vue-amap';
+export default {
+  name: 'amap',
+  data() {
+    return {
+      map: null,
+      zoom: 12,
+    };
+  },
+  mounted() {
+    lazyAMapApiLoaderInstance.load().then(() => {
+      // your code ...
+      this.map = new AMap.Map('amapContainer', {
+        center: [116.404765, 39.918052],
+        zoom: this.zoom,
+      });
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.amap-wrap {
+  height: 100vh;
+}
+</style>
---
+++ src/views/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="amap-wrap">
+    <Map />
+  </div>
+</template>
+
+<script>
+import Map from '../amap/index';
+export default {
+  name: 'index',
+  components: {
+    Map,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.amap-wrap {
+  height: 100vh;
+}
+</style>
